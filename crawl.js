01f32115abc3e8b4e8c4f50e837f39cebec16274
G483 <gabrijel.web@gmail.com>
2024-05-29 17:37:44 +0200

    finish normalizeURL and tests

--- crawl.js
+++ crawl.js
@@ -1,7 +1,12 @@
 function normalizeURL(urlString) {
     const urlObject = new URL(urlString)
+    const hostPath = `${urlObject.hostname}${urlObject.pathname}`
 
-    const hostname = urlObject.hostname
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1)
+    }
+
+    return hostPath
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -2,8 +2,22 @@ const {normalizeURL} = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strips protocol', () => {
-    const input = 'https://blog.boot.dev/path'
+    const input = 'https://blog.boot.dev/path/'
     const actual  = normalizeURL(input)
-    const expected = 'something else'
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strips trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual  = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL ignores capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual  = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
